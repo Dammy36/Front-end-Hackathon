6f239a7171e6a57039fc32bb3c1eed389210bb52
Dammy36 <osedee101@gmail.com>
2023-11-24 01:28:18 +0100

    Added links on the menu sections and updated the CSS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const collapsibles = document.querySelectorAll(".collapsibles");
+const collapsibles = document.querySelectorAll(".main-section");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
